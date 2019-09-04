240df86bc938eb1fbd62db12e00d47f59f4a933a
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2019-09-04 11:03:49 +0100

    Update styles - group edition

--- docs/vendors~main.bffb85d5ce333f8ccef3.bundle.js
+++ docs/vendors~main.bffb85d5ce333f8ccef3.bundle.js
@@ -86,4 +86,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",function(){return isPlainObject})}]]);
-//# sourceMappingURL=vendors~main.d06735990c5603f50b92.bundle.js.map
+//# sourceMappingURL=vendors~main.bffb85d5ce333f8ccef3.bundle.js.map
